39701a6ce5d65d64df1b7094bb8fdd8a8f28e630
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-11-30 11:19:27 +0300

    solve task 3

--- store/js/dom.js
+++ store/js/dom.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function createElement(node) {
+    return typeof node === 'string' ? document.createTextNode(node) : createNodeFromObject()
+
+    function createNodeFromObject() {
+        const element = document.createElement(node.name);
+
+        if (typeof node.props === 'object' && node.props !== null) {
+            Object.keys(node.props).forEach(i => element
+                .setAttribute(i, node.props[i]));
+        }
+
+        if (node.childs instanceof Array) {
+            node.childs.forEach(child => {
+                const nodeChild = createElement(child);
+                element.appendChild(nodeChild);
+            })
+        }
+        return element;
+    }
+}
